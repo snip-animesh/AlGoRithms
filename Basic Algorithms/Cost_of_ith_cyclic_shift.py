3e789a2b1cf3f065ff6c00dec86fcc74dcde4213
snip-animesh <83237705+snip-animesh@users.noreply.github.com>
2021-08-07 11:24:04 +0600

    Update Cost_of_ith_cyclic_shift.py

--- Basic Algorithms/Cost_of_ith_cyclic_shift.py
+++ Basic Algorithms/Cost_of_ith_cyclic_shift.py
@@ -1,1 +1,12 @@
+from collections import Counter
+N=int(input())
+lst=[int(x) for x in input().split()]
+cyclick_lst=[]
+for j in range(1,N+1):
+    cyclick_lst.append((lst[j-1]-j+N)%N)
+d=Counter(cyclick_lst)
 
+while True:
+    n=int(input('koto number RCS er jonne cost chao ? -'))
+    x=d.get(n%N,0)
+    print(N-x)
