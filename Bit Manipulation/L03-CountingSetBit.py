5edb19aab826bd259bacd0f59e9384d048449bda
snip-animesh <83237705+snip-animesh@users.noreply.github.com>
2021-09-04 18:38:47 +0600

    Update L03-CountingSetBit.py

--- Bit Manipulation/L03-CountingSetBit.py
+++ Bit Manipulation/L03-CountingSetBit.py
@@ -5,3 +5,12 @@ while n!=0:
         cnt+=1
     n=n>>1
 print(cnt)
+
+# Complexity O(logn) of both coz each time we are dividing a number by 2 
+
+n=int(input(),2)
+cnt=0
+while n>0:
+    cnt+=1
+    n=n&(n-1)
+print(cnt)
